4b291a3abd5192c3a3a5f61c63db751c470316d0
Carlos <carlos.alfonso.montero@gmail.com>
2021-08-29 15:24:44 -0400

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,7 +3,7 @@ class BirdsController < ApplicationController
   # GET /birds
   def index
     birds = Bird.all
-    render json: birds
+    render json: birds, except: [:created_at, :updated_at]
   end
 
   # POST /birds
@@ -16,16 +16,38 @@ class BirdsController < ApplicationController
   def show
     bird = Bird.find_by(id: params[:id])
     if bird
+      render json: bird, except: [:created_at, :updated_at]
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird, except: [:created_at, :updated_at]
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
       render json: bird
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
 
+  
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
